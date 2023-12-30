d14c260270c787f501b5f1ce746c53e3c3359e5b
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-30 17:34:42 +0200

    unique-corrected

--- salaryApp.js
+++ salaryApp.js
@@ -225,7 +225,7 @@ const uniquifyNames = function (items) {
   const uniqueNames = {};
 
   return items.map(function (item) {
-    if (uniqueNames[item.name]) {
+    if (uniqueNames[item.name] !==undefined) {
       uniqueNames[item.name] += " ";
       item.name += uniqueNames[item.name];
     } else {
